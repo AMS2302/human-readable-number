be6fb4113dd1b01c8616dc1e303f34b611da0dea
AMS2302 <ams23post@gmail.com>
2021-03-14 23:52:17 +0300

    init: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let min = [];
+    min = ["zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen"];
+    if (number < 20) {
+        return min[number];
+    }
+    let tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    if (number < 100) {
+        if (number % 10 == 0) {
+            return tens[(number / 10) - 2];
+        }
+        else {
+            return tens[Math.floor(number / 10) - 2] + " " + min[number % 10];
+        }
+    }
+    else {
+        if (number % 100 == 0) {
+            return min[Math.floor(number / 100)] + " hundred";
+        }
+        if (number % 100 < 20) {
+            return min[Math.floor(number / 100)] + " hundred " + min[number % 100];
+        }
+        if (number % 10 == 0) {
+            return min[Math.floor(number / 100)] + " hundred " + tens[((number - (Math.floor(number / 100) * 100)) / 10) - 2];
+        }
+        else {
+            return min[Math.floor(number / 100)] + " hundred " + tens[Math.floor((number - ((Math.floor(number / 100) * 100))) / 10) - 2] +" " + 
+            min[number % 10];
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
